1d56b43aada49321ff25c0482e83522e7ba3b049
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2022-12-19 20:20:05 -0300

    Adicionando o flyway

--- algamoney-api/src/main/java/com/example/algamoney/AlgamoneyApiApplication.java
+++ algamoney-api/src/main/java/com/example/algamoney/AlgamoneyApiApplication.java
@@ -1,9 +1,12 @@
 package com.example.algamoney;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
