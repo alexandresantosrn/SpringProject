aaba8c59d1c415f984cef338ef834d49afcd9fa9
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2022-12-19 23:24:08 -0300

    Melhorando a inserção de categorias com retorno no location

--- algamoney-api/src/main/java/com/example/algamoney/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/resource/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.example.algamoney.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.model.Categoria;
 import com.example.algamoney.repository.CategoriaRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categorias")
@@ -25,11 +29,16 @@ public class CategoriaResource {
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public void criar(@RequestBody Categoria categoria) {
-		categoriaRepository.save(categoria);
+	public void criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+		response.setHeader("Location", uri.toASCIIString());
 	}
 
 }
