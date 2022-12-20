173a50945dcf140de0d6201406dae16c52945dfc
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2022-12-20 01:35:48 -0300

    Buscando categoria pelo id

--- algamoney-api/src/main/java/com/example/algamoney/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/resource/CategoriaResource.java
@@ -2,11 +2,13 @@ package com.example.algamoney.resource;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +43,13 @@ public class CategoriaResource {
 		
 		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo);		
+	}
+	
+	
+	
 
 }
