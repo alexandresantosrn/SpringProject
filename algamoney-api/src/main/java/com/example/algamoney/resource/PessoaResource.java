276c8cb64039f0f1a3b0f89749dff207a0b96594
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-01-09 08:35:43 -0300

    Tentando registrar pessoa via Postman

--- algamoney-api/src/main/java/com/example/algamoney/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/resource/PessoaResource.java
@@ -1,14 +1,41 @@
 package com.example.algamoney.resource;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.algamoney.model.Pessoa;
 import com.example.algamoney.repository.PessoaRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaResource {
 
+	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+
 }
