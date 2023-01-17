9845d7d2fe3ab7b5b0e93eb0bb5bbf382c78969d
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-01-16 22:59:55 -0300

    Criando RecursoCriadoEvent

---
+++ algamoney-api/src/main/java/com/example/algamoney/event/RecursoCriadoEvent.java
@@ -0,0 +1,36 @@
+package com.example.algamoney.event;
+
+import org.springframework.context.ApplicationEvent;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	private static final long serialVersionUID = 1L;
+
+	private HttpServletResponse response;
+	private long codigo;
+
+	public RecursoCriadoEvent(Object source) {
+		super(source);
+		this.response = response;
+		this.codigo = codigo;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+}
